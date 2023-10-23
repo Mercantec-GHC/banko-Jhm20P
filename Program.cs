8189a2bf5d5be428d21503a71702a638dd00955d
Jonas Mikkelsen <jmikkelsen40@outlook.com>
2023-10-23 12:40:41 +0200

    first attempt

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.WriteLine("Banko");
+Random number = new Random();
+int firstRow = number.Next(0, 91);
+int seccondRow = number.Next(0, 91);
+int thirdRow = number.Next(0, 91);
+int[,] numbers = new int[3, 9];
+for (int i = 0; i<3; i++){
+    for (int j = 0; j < 9; j++){
+        System.Console.WriteLine(numbers[i, j]);
+    }
+}
